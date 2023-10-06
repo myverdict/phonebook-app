ae807ece0304ef20febf28c40f622b5196a9e31c
Sam Vitare <sandhya.palanki@gmail.com>
2023-10-06 11:59:39 -0500

    completed part 3a, exercise 3.8, step 8 phonebook backend

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const morgan = require("morgan");
 const express = require("express");
 // a function that is used to create an express application stored in the app variable
 const app = express();
+
+// https://stackoverflow.com/a/55848217/9621971
+morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 let persons = [
   {
