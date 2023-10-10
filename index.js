7df1cccec999c6cc8c2bace45051cc56bb5e7ba2
Sam Vitare <sandhya.palanki@gmail.com>
2023-10-09 23:00:38 -0500

    working phonebook app

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(cors());
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
+app.use(express.static("dist"));
 
 let persons = [
   {
