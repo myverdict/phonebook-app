6ce376156180cb0c4d977cbb00e61928ca53f643
Sam Vitare <sandhya.palanki@gmail.com>
2023-10-12 15:41:31 -0500

    completed part 3c, exercise 3.12 command line database, phonebook backend

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3] || undefined;
+const number = process.argv[4] || undefined;
+
+const uri = `mongodb+srv://phone:${password}@cluster0.slurjbk.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(uri);
+
+const phonebookSchema = mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", phonebookSchema);
+
+if (name && number) {
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((persons) => {
+    console.log("phonebook: ");
+    persons.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+
+    mongoose.connection.close();
+  });
+}
