95240030509ff1ff1da69e5926c8c0038c5885f9
Sam Vitare <sandhya.palanki@gmail.com>
2023-10-13 08:55:43 -0500

    completed part 3c, exercise 3.14, step 2 phonebook database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const morgan = require("morgan");
 const express = require("express");
 const cors = require("cors");
-const Phonebook = require("./models/phonebook");
+const Person = require("./models/phonebook");
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(express.static("dist")); // static middleware
 app.get("/info", (request, response) => {
   const date = new Date();
 
-  Phonebook.find({}).then((persons) => {
+  Person.find({}).then((persons) => {
     response.send(
       `<div><p>Phonebook has info for ${persons.length} people</p><p>${date}</p></div>`
     );
@@ -26,50 +26,42 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  Phonebook.find({}).then((persons) => {
+  Person.find({}).then((persons) => {
     response.json(persons);
   });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  Phonebook.findById(request.params.id).then((person) => response.json(person));
+  Person.findById(request.params.id).then((person) => response.json(person));
 });
 
-// app.post("/api/persons", (request, response) => {
-//   const body = request.body;
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
-//   if (!body.name && !body.number) {
-//     return response.status(400).json({
-//       error: "name and number missing",
-//     });
-//   } else if (!body.name) {
-//     return response.status(400).json({
-//       error: "name missing",
-//     });
-//   } else if (!body.number) {
-//     return response.status(400).json({
-//       error: "number missing",
-//     });
-//   }
+  if (!body.name && !body.number) {
+    return response.status(400).json({
+      error: "name and number missing",
+    });
+  } else if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
 
-//   const isFound = persons.find(
-//     (person) => person.name.toLowerCase() === body.name.toLowerCase()
-//   );
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
 
-//   if (isFound) {
-//     return response.status(400).json({
-//       error: "name already exists. name must be unique",
-//     });
-//   }
-
-//   const person = {
-//     name: body.name,
-//     number: body.number,
-//     id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER), // 9007199254740991
-//   };
-//   persons = persons.concat(person);
-//   response.json(person);
-// });
+  person.save().then((savedPerson) => {
+    console.log("person saved");
+    response.json(savedPerson);
+  });
+});
 
 // app.delete("/api/persons/:id", (request, response) => {
 //   const id = Number(request.params.id);
